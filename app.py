11c660cd09cba58171e53d98f247aa3efcd41b26
isiko1 <lutersweg@hotmail.com>
2020-05-01 23:19:35 +0000

    Added js to base.html, modified patients.html and app.py

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists('env.py'):
+    import env
 
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = "dental_laboratory"
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World'
+@app.route('/get_patients')
+def get_patients():
+    return render_template('patients.html', patients=mongo.db.patients.find())
+
+
+@app.route('/add_patient')
+def add_patient():
+    return render_template('addpatient.html')
 
 
 if __name__ == '__main__':
