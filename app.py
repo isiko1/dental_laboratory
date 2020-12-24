4fd814f6098968065bdfe9de71bc30d4edd5558d
isiko1 <lutersweg@hotmail.com>
2020-12-24 14:59:00 +0000

    ammended config and path

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from forms import LoginForm
 from os import path
-if path.exists('env.py'):
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = "dental_laboratory"
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.config['SECRET_KEY'] = 'MONGO_URI'
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 # ---------------------------------------Login----------------------------
