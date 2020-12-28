42a2a6fd1290a6b65b0bf06c4a50033c913703ff
isiko1 <lutersweg@hotmail.com>
2020-12-28 16:07:49 +0000

    Added static js

---
+++ static/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+        $('.collapsible').collapsible();
+        $('select').material_select();
+        $('.sidenav').sidenav();
+      });
+      /* ----Code from mini project------*/
+      $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 100, // Creates a dropdown of 80 years to control year,
+        today: "Today",
+        clear: "Clear",
+        close: "Ok",
+        closeOnSelect: false, // Close upon selecting a date,
+      });
